3953a04dc536684085bacb8297de82bab4431343
aymen-jlassi <aymentunis08@gmail.com>
2020-12-31 18:02:00 +0100

    delete is working

--- back/routes/vente.js
+++ back/routes/vente.js
@@ -40,12 +40,12 @@ venteRoute.get('/all', function(req,res){
   
    });
 //// delete
-   venteRoute.delete('/btb', function(req,res){
+   venteRoute.delete('/delete/:id', function(req,res){
     odoo.connect(function (err) {
       if (err) { return console.log(err); }
       console.log('connected ! ');
     });
-    const id = 4;
+   var id = parseInt(req.params.id);
     odoo.delete('sale.order', id, function (err, products) {
       if (err) { return console.log(err); }
       res.json(products);
